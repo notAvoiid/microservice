d59bd5b723f8c7f05ccd571fcbc706bb9ba69f93
Igor <igor.nacimento51@gmail.com>
2023-06-22 20:55:00 -0300

    "Connecting the application with Eureka"

---
+++ naming-service/src/test/java/br/com/mrzoom/namingservice/NamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrzoom.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
