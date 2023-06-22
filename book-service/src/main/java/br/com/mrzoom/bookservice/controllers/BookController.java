8f408728651ff6ea183db2257a505dac2aac510f
Igor <igor.nacimento51@gmail.com>
2023-06-22 20:05:28 -0300

    "Setting book-service and returning mocks"

---
+++ book-service/src/main/java/br/com/mrzoom/bookservice/controllers/BookController.java
@@ -0,0 +1,30 @@
+package br.com.mrzoom.bookservice.controllers;
+
+import br.com.mrzoom.bookservice.models.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/book-service")
+public class BookController {
+
+    @Autowired
+    private Environment environment;
+
+    // http://localhost:8100/book-service/1/BRL
+    @GetMapping(value = "{id}/{currency}")
+    public Book findBook(@PathVariable("id")Long id, @PathVariable("currency")String currency){
+
+        var port = environment.getProperty("local.server.port");
+
+        return new Book(1L, "Igor", "Docker", new Date(),
+                50.7, currency, port);
+    }
+
+}
---
+++ book-service/src/main/java/br/com/mrzoom/bookservice/models/Book.java
@@ -0,0 +1,104 @@
+package br.com.mrzoom.bookservice.models;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Book implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String author;
+    private Date launchDate;
+    private Double price;
+    private String title;
+    private String currency;
+    private String environment;
+
+    public Book(){}
+
+    public Book(Long id, String author, String title, Date launchDate, Double price
+            , String currency, String environment) {
+        this.id = id;
+        this.author = author;
+        this.launchDate = launchDate;
+        this.price = price;
+        this.title = title;
+        this.currency = currency;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLaunchDate() {
+        return launchDate;
+    }
+
+    public void setLaunchDate(Date launchDate) {
+        this.launchDate = launchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
