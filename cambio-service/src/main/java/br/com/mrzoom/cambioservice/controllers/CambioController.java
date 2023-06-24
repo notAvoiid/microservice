1ab99b3dff770b2eb87bb1286e10f855ff9910fb
Igor <igor.nacimento51@gmail.com>
2023-06-24 14:23:26 -0300

    "Setting SpringDoc OpenApi UI for book-service and cambio-service"

--- cambio-service/src/main/java/br/com/mrzoom/cambioservice/controllers/CambioController.java
+++ cambio-service/src/main/java/br/com/mrzoom/cambioservice/controllers/CambioController.java
@@ -2,6 +2,8 @@ package br.com.mrzoom.cambioservice.controllers;
 
 import br.com.mrzoom.cambioservice.models.Cambio;
 import br.com.mrzoom.cambioservice.repositories.CambioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+@Tag(name = "Cambio service API")
 @RestController
 @RequestMapping("/cambio-service")
 public class CambioController {
@@ -23,6 +26,7 @@ public class CambioController {
     private CambioRepository repository;
 
     // http://localhost:8000/cambio-service/5/USD/BRL
+    @Operation(summary = "Get cambio from currency")
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
             @PathVariable("amount")BigDecimal amount,
