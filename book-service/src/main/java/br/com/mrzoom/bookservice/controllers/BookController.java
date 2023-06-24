94a8750222bdf308752ddf17cfdebb098fd9519c
Igor <igor.nacimento51@gmail.com>
2023-06-24 14:12:48 -0300

    "Adding swagger and setting in book-service"

--- book-service/src/main/java/br/com/mrzoom/bookservice/controllers/BookController.java
+++ book-service/src/main/java/br/com/mrzoom/bookservice/controllers/BookController.java
@@ -3,6 +3,8 @@ package br.com.mrzoom.bookservice.controllers;
 import br.com.mrzoom.bookservice.models.Book;
 import br.com.mrzoom.bookservice.proxy.CambioProxy;
 import br.com.mrzoom.bookservice.repositories.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
-
+@Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("/book-service")
 public class BookController {
@@ -27,6 +29,7 @@ public class BookController {
     private CambioProxy proxy;
 
     // http://localhost:8100/book-service/1/BRL
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "{id}/{currency}")
     public Book findBook(@PathVariable("id")Long id, @PathVariable("currency")String currency){
 
--- book-service/src/main/java/br/com/mrzoom/bookservice/controllers/FooBarController.java
+++ book-service/src/main/java/br/com/mrzoom/bookservice/controllers/FooBarController.java
@@ -1,6 +1,8 @@
 package br.com.mrzoom.bookservice.controllers;
 
 import io.github.resilience4j.retry.annotation.Retry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Tag(name = "Foo-bar")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
 
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
+    @Operation(summary = "Foo bar")
     @GetMapping("/foo-bar")
     @Retry(name = "default", fallbackMethod = "fallbackMethod")
     public String fooBar(){
